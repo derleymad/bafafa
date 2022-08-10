dfd4244b559deaf3124760db4addf2258e04d845
derleymad <filho.wanderley@hotmail.com>
2022-08-10 14:45:09 -0300

    adding event text change

--- app/src/main/java/com/github/derleymad/bafafa/login/view/LoginActivity.kt
+++ app/src/main/java/com/github/derleymad/bafafa/login/view/LoginActivity.kt
@@ -2,11 +2,40 @@ package com.github.derleymad.bafafa.login.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import com.github.derleymad.bafafa.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val editTextEmail = findViewById<TextInputEditText>(R.id.login_edit_email)
+        val editTextPassword = findViewById<TextInputEditText>(R.id.login_edit_password)
+
+        editTextEmail.addTextChangedListener(watcher)
+        editTextPassword.addTextChangedListener(watcher)
+
+        findViewById<Button>(R.id.login_btn_enter).setOnClickListener{
+            findViewById<TextInputLayout>(R.id.login_txt_input_layout_email).error =  "E-mail incorreto"
+            findViewById<TextInputLayout>(R.id.login_txt_input_layout_password).error =  "Senha incorreta"
+        }
+    }
+
+    private val watcher = object : TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                findViewById<Button>(R.id.login_btn_enter).isEnabled = s.toString().isNotEmpty()
+        }
+        override fun afterTextChanged(s: Editable?) {
+        }
     }
 }
